49f8e8720611e5860ffe1aace91d20467d2c1c0c
Documenter.jl <documenter@juliadocs.github.io>
2025-06-06 11:06:30 +0000

    build based on 69e1a0f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SimplSurfs","page":"Home","title":"SimplSurfs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SimplSurfs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
