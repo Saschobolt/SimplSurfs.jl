31430386ec9b61791214bc7f693d796b68ce4235
Sascha Stüttgen <sascha.stuettgen@rwth-aachen.de>
2025-09-02 11:23:44 +0200

    examples

--- src/SimplSurfs.jl
+++ src/SimplSurfs.jl
@@ -4,7 +4,10 @@ using Graphs
 using AbstractAlgebra
 using StaticArrays
 
-export Vertex, Face, Edge, PolyhedralMesh, make_edge, head, head!, tail, tail!, left, left!, right, right!, flip, rot, invrot, next, lnext, rnext, mesh, splice!, edge, is_primary, is_dual, splice!, vertices, faces, primal_edges, dual_edges, prev, is_boundary
+export Vertex, Face, Edge, PolyhedralMesh, make_edge, head, head!, tail, tail!, left, left!, right, right!, flip, rot, invrot, next, lnext, rnext, mesh, splice!, edge, is_primary, is_dual, splice!, vertices, faces, edges, dual_edges, prev, is_boundary
 include("PolyhedralMesh.jl")
+
+export octahedron, tetrahedron, double_tetrahedron
+include("examples.jl")
 
 end # module SimplSurfs
---
+++ src/examples.jl
@@ -0,0 +1,3 @@
+octahedron() = PolyhedralMesh([[1, 2, 3], [1, 3, 4], [1, 4, 5], [1, 2, 5], [6, 2, 3], [6, 3, 4], [6, 4, 5], [6, 2, 5]])
+tetrahedron() = PolyhedralMesh([[1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4]])
+double_tetrahedron() = PolyhedralMesh([[1, 2, 3], [1, 2, 4], [1, 3, 4], [5, 2, 3], [5, 2, 4], [5, 3, 4]])
